8b4ee8392d7c455cc6d7b80844063c12fcdb6c6f
Paviotti <crpaviotti@hotmail.com>
2023-06-16 18:25:16 -0300

    Site no git pela primeira vez

---
+++ js/especialidades.js
@@ -0,0 +1,9 @@
+let esp001="Clinica Geral"
+let med001="Dra. Bruna Ribeiro"
+let esp002="Ortopedista"
+let med012="Dr. Leandro Ferreira"
+let med022="Dr. Rogério Bastos"
+let esp003= "Nitricionista"
+let med003= "Dra. Wandeci Olivia"
+
+// document.getElementById("clinica_g").innerHTML=esp001
